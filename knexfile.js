c5fb4f55e862d0a0037911f925daa2cd0bb71013
Angel Hernandez <Angelh289@gmail.com>
2021-11-14 23:52:05 -0800

    connecting database

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://wvhgjwvh:R-ofLb95rrQ9v52u1vQFzsgFQlTY6oZB@fanny.db.elephantsql.com/wvhgjwvh",
 } = process.env;
 
 module.exports = {
